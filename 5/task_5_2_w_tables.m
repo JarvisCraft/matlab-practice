d9833e9bf84db94cd869c39e881edd4e349ad1f9
progrm_jarvis <gh@progrm-jarvis.ru>
2020-07-11 03:59:15 +0300

    Add task 5

---
+++ 5/task_5_2_w_tables.m
@@ -0,0 +1,72 @@
+% Задаю общие константы
+Um = 12;
+f = 50;
+omega = 2 * pi * f;
+
+% Задаю функцию U_В
+UB = @(t) abs(Um * cos(omega * t));
+
+% Устанавливаю исследуемый диапазон
+range = 0 : 0.00001 : 0.1;
+
+% Строю график функции U_В
+figure(1);
+plot(range, UB(range));
+grid on;
+title('$U_B(t)$', 'Interpreter', 'latex')
+xlabel('t', 'Interpreter', 'latex');
+ylabel('$U_B$', 'Interpreter', 'latex');
+
+% Наборы значений по вариантам
+R0s = [20 75 35 120 90 20];
+RHs = [500 1000 750 1000 2000 1000];
+Ls = [0.5 0.7 0.1 1 1.2 0.05];
+Cs = [100 40 200 50 100 1000];
+
+% Перебор заданий
+for variant = 1 : 6    
+    % Задаю известные значения
+    R0 = R0s(variant);
+    RH = RHs(variant);
+    L = Ls(variant);
+    C = Cs(variant);
+
+    % Задаю функции du/dt и di/dt
+    dudt = @(Y) 1 / C * (Y(2) - Y(1) / RH);
+    didt = @(t, Y) 1 / L * (UB(t) - Y(2) * R0 - Y(1));
+
+    % Объединяю dudt и didt в единую function handle
+    dudidt = @(t, Y) [dudt(Y) didt(t, Y)]';
+    [T, UI] = ode45(dudidt, [0, 1], [0, 0]);
+
+    % Строю граифик U(t)
+    figure(2);
+    plot(T, UI(:, 1));
+    title('$U(t)$', 'Interpreter', 'latex')
+    grid on;
+    xlabel('t', 'Interpreter', 'latex');
+    ylabel('$U$', 'Interpreter', 'latex');
+
+    % Строю граифик i(t)
+    figure(3);
+    plot(T, UI(:, 2));
+    title('$i(t)$', 'Interpreter', 'latex')
+    grid on;
+    xlabel('t', 'Interpreter', 'latex');
+    ylabel('$i$', 'Interpreter', 'latex');
+
+    fprintf('|——————————————————————————|\n');
+    fprintf('|    t   |  U(t)  |  i(t)  |\n');
+    fprintf('|--------|--------|--------|\n')
+    i = 0;
+
+    % транспонирую матрицы для удобства перебора
+    for t = T'
+        i = i + 1;
+        fprintf('|%f|%f|%f|\n', t, UI(i, 1), UI(i, 2));
+    end
+    fprintf('|——————————————————————————|\n')
+
+    % Приостанавливаю исполнение программы для удобства копирования таблицы
+    pause
+end
