c00455a9aaec83319f894407a25f03c9d11d874b
PROgrammer_JARvis <gh@progrm-jarvis.ru>
2020-07-11 10:59:06 +0300

    Fix existing issues

--- 4/print_complex_int_data.m
+++ 4/print_complex_int_data.m
@@ -1,9 +1,9 @@
 function print_complex_int_data(name, I0, I, n, h)
     fprintf('%s:\n', name);
-    fprintf('\tШаг интегрирования: %d\n', h);
-    fprintf('\tКол-во узлов интегрирования: %d\n', n);
-    fprintf('\tЗначение: %d\n', I);
+    fprintf('\tШаг интегрирования: %f\n', h);
+    fprintf('\tКол-во узлов интегрирования: %i\n', n);
+    fprintf('\tЗначение: %f\n', I);
     D = abs(I0 - I);
-    fprintf('\tАбс. погрешность: %d\n', D);
-    fprintf('\tОтн. погрешность: %d\n', D ./ I);
+    fprintf('\tАбс. погрешность: %f\n', D);
+    fprintf('\tОтн. погрешность: %f\n', D ./ I);
 end
--- 4/print_int_data.m
+++ 4/print_int_data.m
@@ -1,9 +1,9 @@
 function print_int_data(name, I0, I, R)
     fprintf('%s:\n', name);
-    fprintf('\tЗначение: %d\n', I);
+    fprintf('\tЗначение: %f\n', I);
     D = abs(I0 - I);
-    fprintf('\tАбс. погрешность: %d\n', D);
-    fprintf('\tОтн. погрешность: %d\n', D ./ I);
-    fprintf('\tТеор. абс. погрешность: %d\n', R);
-    fprintf('\tТеор. отн. погрешность: %d\n', R ./ I);
+    fprintf('\tАбс. погрешность: %f\n', D);
+    fprintf('\tОтн. погрешность: %f\n', D ./ I);
+    fprintf('\tТеор. абс. погрешность: %f\n', R);
+    fprintf('\tТеор. отн. погрешность: %f\n', R ./ I);
 end
